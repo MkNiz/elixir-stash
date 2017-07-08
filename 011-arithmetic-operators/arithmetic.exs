79a8ddcc171ff45fdd26301a0e96ffd001887ee1
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-08 13:54:17 -0400

    011 - Arithmetic Operators

---
+++ 011-arithmetic-operators/arithmetic.exs
@@ -0,0 +1,14 @@
+IO.puts "5 + 3 = "
+IO.puts 5 + 3
+IO.puts "5 - 3 = "
+IO.puts 5 - 3
+IO.puts "5 * 3 = "
+IO.puts 5 * 3
+IO.puts "5 / 3 = "
+IO.puts 5 / 3
+IO.puts "5 / 3 with an integer result = "
+IO.puts div(5, 3)
+IO.puts "Remainder of 5 / 3 = "
+IO.puts rem(5, 3)
+IO.puts "Remainder of -5 / 3 = "
+IO.puts rem(-5, 3)
---
+++ 011-arithmetic-operators/readme.md
@@ -0,0 +1,4 @@
+# Arithmetic Operators
+
+Demonstrates the use of arithmetic operators in Elixir, including
+div() and rem().
