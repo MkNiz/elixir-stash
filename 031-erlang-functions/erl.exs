211ad8f567fa767d8e9f3c71bf42c8685617c9a9
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-15 10:05:48 -0400

    031 - Erlang Functions

---
+++ 031-erlang-functions/erl.exs
@@ -0,0 +1,2 @@
+IO.puts "Absolute value of -5: #{:erlang.abs(-5)}"
+IO.puts "Third element of {5, 1, 3, 4}: #{:erlang.element(3, {5, 1, 3, 4})}"
---
+++ 031-erlang-functions/readme.md
@@ -0,0 +1,4 @@
+# Erlang Functions
+
+Demonstrates that Elixir can utilize Erlang functions by calling them as
+atoms.
